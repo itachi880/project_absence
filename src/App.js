007dd1dd8412ca003a4f0642e8dc4e2f6736ba0f
itachi880 <bdortiana880@gmail.com>
2024-10-29 19:58:43 +0100

    add axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import NavBar from "./NavBar";
 import Login from "./Login";
-
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { userDataStore } from "./data";
+import { useEffect } from "react";
 function App() {
+  const navigator = useNavigate();
+  const [userData, _] = userDataStore.useStore();
+  useEffect(() => {
+    if (!userData.token) navigator("/login");
+  }, [userData]);
   return (
     <div className="App">
       <NavBar />
-      <Login />
+      <Routes>
+        <Route element={<Login />} path="/login" />
+      </Routes>
     </div>
   );
 }
--- src/Login/index.js
+++ src/Login/index.js
@@ -1,34 +1,47 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./index.css";
 export default function () {
   const [formData, setFormData] = useState({
     login: "",
     password: "",
   });
+  const [passwordVisible, setPassVisibility] = useState(false);
+  const inputsControle = {
+    password: useRef(""),
+  };
   return (
     <div className="login">
       <h1>Sign in</h1>
       <div className="form">
         <div className="input">
+          <span className={`input-titel ${formData.login.trim().length > 0 ? "input-full" : ""}`}>Email</span>
           <input
             type="text"
             value={formData.login}
             onChange={(e) => {
               setFormData({ ...formData, login: e.target.value });
             }}
-            placeholder="Email"
           />
         </div>
-        <div className="input">
+        <div className="input password">
           <input
-            type="text"
+            type={passwordVisible ? "text" : "password"}
+            ref={inputsControle.password}
             value={formData.password}
             onChange={(e) => {
               setFormData({ ...formData, password: e.target.value });
             }}
-            placeholder="Password"
           />
-          <span>Show</span>
+          <span className={`input-titel ${formData.password.trim().length > 0 ? "input-full" : ""}`}>Password</span>
+          <span
+            onClick={(e) => {
+              console.log(e.target.innerHTML.toLowerCase());
+              if (passwordVisible) return setPassVisibility(false);
+              setPassVisibility(true);
+            }}
+          >
+            {passwordVisible ? "Hide" : "Show"}
+          </span>
         </div>
         <div className="options">
           <span>Forgot password?</span>
