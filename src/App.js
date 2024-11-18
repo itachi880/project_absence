9e30f083935a125265d8b7ecd641620ec3db9fe6
itachi880 <bdortiana880@gmail.com>
2024-11-18 21:54:25 +0100

    Co-authored-by: iliass7777 <iliass7777@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import NavBar from "./NavBar";
 import Login from "./Login";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import { jwt_token, userDataStore } from "./data";
+import { jwt_token, loadingFlag, userDataStore } from "./data";
 import { useEffect } from "react";
 import { Store } from "react-data-stores";
 import { auth_token } from "./api";
@@ -12,6 +12,7 @@ import GroupsRoute from "./Routes/GroupsRoute";
 function App() {
   Store.navigateTo = useNavigate();
   const [userData, setUserData] = userDataStore.useStore();
+  const [loading, setLoadingFlag] = loadingFlag.useStore();
   async function checkUser_JWT_Localstorage() {
     let [error, data] = [null, null];
     if (!localStorage.getItem(jwt_token)) return [true, null];
@@ -34,6 +35,7 @@ function App() {
   }, [userData]);
   return (
     <div className="App">
+      <LoadingBar active={loading.state} />
       <NavBar />
 
       {/* <BarChart
@@ -107,3 +109,31 @@ function App() {
 }
 
 export default App;
+function LoadingBar({ active = false }) {
+  return (
+    <div id="loading-bar-container">
+      <style>
+        {`
+          #loading-bar-container .loading-bar {
+            position: absolute;
+            top: 0;
+            height: 5px;
+            z-index: 1000;
+            background-color: blue;
+          }
+          #loading-bar-container .loading-bar.active {
+            animation: loading-bar-animation 1s linear infinite;
+          }
+          @keyframes loading-bar-animation {
+            0% { left: 0; width: 0; }
+            25% { left: 0; width: 40%; }
+            50% { left: 30%; width: 30%; }
+            75% { left: 70%; width: 10%; }
+            100% { left: 100%; width: 0; }
+          }
+        `}
+      </style>
+      <div className={`loading-bar ${active ? "active" : ""}`}></div>
+    </div>
+  );
+}
--- src/Routes/UsersRoute/ShowUserByID/index.js
+++ src/Routes/UsersRoute/ShowUserByID/index.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "./index.css";
-import { absences, GroupsDataStore, justificationsStore, jwt_token, studentsByGroup, userDataStore } from "../../../data";
+import { absences, GroupsDataStore, justificationsStore, jwt_token, loadingFlag, roles, statusCertif, studentsByGroup, userDataStore } from "../../../data";
 import { getGroupByID, getJustificationByID, getUserAbsenceByID, getUsersByGroupID } from "../../../api";
 import { spans } from "../../../utils";
 
@@ -19,21 +19,30 @@ export default function () {
   const [selectedDate, setSelectedDate] = useState("");
   const [selectedDateObj, setSelectedDateObj] = useState(new Date());
   const [groups, setGroups] = GroupsDataStore.useStore();
+  const [loading, setLoadingFlag] = loadingFlag.useStore();
 
   useEffect(() => {
     if (!studentsData[group]) {
+      setLoadingFlag({ state: true });
       getUsersByGroupID(group, userData.token).then(async (res) => {
-        if (res[0]) return console.log(res[0]);
+        if (res[0]) {
+          console.log(res[0]);
+          setLoadingFlag({ state: false });
+          return;
+        }
         const [error, data] = await getGroupByID(userData.token, group);
         setStudentsData({ [group]: res[1].data }, false); // false = add // true = update
-        if (error) return;
+        if (error) return setLoadingFlag({ state: false });
+        setLoadingFlag({ state: false });
         setGroups({ groups: [...groups.groups, data] });
       });
     }
     const studentAbsence = studentsData[group]?.find((obj) => obj._id === id)?.absences || [];
     if (studentAbsence.length === 0) return;
     if (absencesStore.data.fi) return; // to do ??
+    setLoadingFlag({ state: true });
     getUserAbsenceByID(localStorage.getItem(jwt_token), studentAbsence[studentAbsence.length - 1]).then((res) => {
+      setLoadingFlag({ state: false });
       if (res[0]) return console.log(res[0]);
       setAbsencesStore({ data: [res[1]] }, false);
     });
@@ -131,9 +140,20 @@ export default function () {
 
 const ShowUser = ({ studentsData, groups, group, id }) => {
   const d = studentsData[group]?.filter((student) => student._id === id)[0];
+  const [loading, setLoadingFlag] = loadingFlag.useStore();
+
+  useEffect(() => {
+    setLoadingFlag({ state: true });
+  }, []);
   if (!d) return null;
+
   return (
-    <div className="user-card">
+    <div
+      className="user-card"
+      onLoad={() => {
+        setLoadingFlag({ state: false });
+      }}
+    >
       <div className="student-info">
         <div className="student-profile">
           <img src={require("../../../assets/no-profile-picture-icon.webp")} />
@@ -168,64 +188,91 @@ function ShowJustifCard({ datesToMark = [], selectedDate = "" }) {
   const certif = datesToMark.find((date) => {
     return date.date == selectedDate;
   });
+  const [loading, setLoadingFlag] = loadingFlag.useStore();
   const [certifStore, setCertifStore] = justificationsStore.useStore();
   const [selectedCertif, setSelectedCertif] = useState(null);
+  const [userData, setUserData] = userDataStore.useStore();
+  const [isHovered, setIsHovered] = useState(false);
   useEffect(() => {
     if (!certif?.justif_id) return;
     const index = certifStore.data.findIndex((item) => item._id == certif?.justif_id);
     //ila l9it certif deja kayna f base local manjibohach mn base donne fserveur dik index ila kant -1 ya3ni certif makaynach ila kant 0+ m3naha l9inaha
     if (index > -1) return setSelectedCertif(certifStore.data[index]);
+    setLoadingFlag({ state: true });
     getJustificationByID(certif?.justif_id, localStorage.getItem(jwt_token)).then((res) => {
       if (res[0] || !res[1]?._id) return;
       setSelectedCertif(res[1]);
 
       setCertifStore({ data: [...certifStore.data, res[1]] });
+      setLoadingFlag({ state: false });
     });
   }, [certif]);
 
   return (
     <div className="justifications">
-      {certif?.justif_id ? (
-        <table className="certificate-table">
-          <thead>
-            <tr>
-              <th>Details</th>
-              <th>Information</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Start Date</td>
-              <td>{selectedCertif?.start_date.split("T")[0]}</td>
-            </tr>
-            <tr>
-              <td>Days Covered</td>
-              <td>{selectedCertif?.justification_date} Days</td>
-            </tr>
-            <tr>
-              <td>Additional Info</td>
-              <td>{selectedCertif?.details}</td>
-            </tr>
-            {selectedCertif?.media ? (
+      {certif?.justif_id && selectedCertif?.start_date ? (
+        <>
+          <table className="certificate-table">
+            <thead>
               <tr>
-                <td>PDF</td>
-                <td>
-                  <a href={selectedCertif?.media} className="pdf-link" target="_blank">
-                    View PDF
-                  </a>
+                <th>Details</th>
+                <th>Information</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Start Date</td>
+                <td>{selectedCertif?.start_date.split("T")[0]}</td>
+              </tr>
+              <tr>
+                <td>Days Covered</td>
+                <td>{selectedCertif?.justification_date} Days</td>
+              </tr>
+              <tr>
+                <td>Additional Info</td>
+                <td>{selectedCertif?.details}</td>
+              </tr>
+              {selectedCertif?.media ? (
+                <tr>
+                  <td>PDF</td>
+                  <td>
+                    <a href={selectedCertif?.media} className="pdf-link" target="_blank">
+                      View PDF
+                    </a>
+                  </td>
+                </tr>
+              ) : (
+                <tr>
+                  <td colSpan={"2"}>no document</td>
+                </tr>
+              )}
+              <tr>
+                <td>status</td>
+                <td className="status">
+                  {selectedCertif?.status == statusCertif.no_valide ? <spans.false text={"pas valide"} /> : selectedCertif?.status == statusCertif.panding ? <spans.maybe text={"en cours"} /> : <spans.true text={"valide"} />}
+
+                  {userData.data.role == roles.generale_survience ? (
+                    <>
+                      <div className="icon-container" onClick={() => setIsHovered((e) => !e)}>
+                        <i className="fa-solid fa-ellipsis-vertical"></i>
+                      </div>
+                      <div className={`confirm-certif ${isHovered ? "" : "confirm-certif-none"}`}>
+                        <button className="validate-button">validated</button>
+                        <button className="validate-button">not validated</button>
+                      </div>
+                    </>
+                  ) : (
+                    ""
+                  )}
                 </td>
               </tr>
-            ) : (
-              <tr>
-                <td colSpan={"2"}>no document</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        </>
       ) : certif?.events?.length > 0 ? (
-        <p>ce jour est pas justifie</p>
+        <h3>ce jour n'est pas justifie</h3>
       ) : (
-        ""
+        <h3>Not absence this sceance</h3>
       )}
     </div>
   );
