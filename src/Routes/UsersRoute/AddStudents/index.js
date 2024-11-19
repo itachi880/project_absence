617a853d54441a9e272498b9549cc2129527ec0d
itachi880 <bdortiana880@gmail.com>
2024-11-19 12:06:24 +0100

    pagination group1

--- src/Routes/UsersRoute/AddStudents/index.js
+++ src/Routes/UsersRoute/AddStudents/index.js
@@ -1,48 +1,89 @@
 import { useEffect, useRef, useState } from "react";
-import { GroupsDataStore, jwt_token } from "../../../data";
-import { getGroups } from "../../../api/index";
-
+import { GroupsDataStore, jwt_token, loadingFlag } from "../../../data";
+import { addStudent, getGroups } from "../../../api/index";
+import { searchGroupsByName } from "../../../api/index";
+import "./index.css";
 export default function () {
   const [groups, setGroups] = GroupsDataStore.useStore();
   const [formData, setFormData] = useState({
     login: "",
-    password: "",
+    first_name: "",
+    last_name: "",
+    cin: "",
+    group: "",
   });
-  const loadingBarRef = useRef("");
+  const [loading, setLoadingFlag] = loadingFlag.useStore();
   const inputsControle = {
     error_message_info: useRef(""),
+  };
+  const [search, setSearch] = useState({ na: [], keys: ["na"] }); //de:[{},{}],keys:["de","id"],id:[{},{}]
+  const [currentSearch, setCurrentSearch] = useState([{ name: "", _id: "" }]);
+  const [fetchFlag, setFetchFlag] = useState(false);
+  const handleSearch = async (value) => {
+    if (value.trim().length < 2) return;
+    setFetchFlag(true);
+    const result = search[search.keys.find((key) => value.toLowerCase().trim().includes(key.toLocaleLowerCase()))]?.filter((obj) => obj.name.toLowerCase().includes(value.toLowerCase())) || [];
+
+    if (result.length > 0) {
+      console.log("Search results local:", result);
+      setCurrentSearch(result.map((e) => ({ name: e.name, _id: e._id })));
+      setFetchFlag(false);
+      return;
+    }
+    const [error, data] = await searchGroupsByName(value);
+
+    if (error) {
+      setFetchFlag(false);
+      console.error("Error searching groups:", error);
+      return;
+    }
+    setSearch({ ...search, [value.toLowerCase().trim()]: data, keys: [...search.keys, value.toLowerCase().trim()] });
+    console.log("Search results server:", data);
+    setCurrentSearch(data.map((e) => ({ name: e.name, _id: e._id })));
+
+    setFetchFlag(false);
   };
   useEffect(() => {
     getGroups(window.localStorage.getItem(jwt_token), false).then((res) => {
       if (res[0]) return;
-      setGroups({ groups: res[1] }, false);
+      const exist = [];
+      setGroups(
+        {
+          groups: [...res[1].groups, ...groups.groups].filter((group) => {
+            if (!exist.includes(group._id)) {
+              exist.push(group._id);
+              return true;
+            }
+          }),
+        },
+        false
+      );
     });
   }, []);
+
   return (
-    <div className="login">
-      <div className="loading-bar" ref={loadingBarRef}></div>
+    <div className="add-student">
       <div>
         <span className="error" ref={inputsControle.error_message_info}></span>
         <h1>inscription</h1>
       </div>
       <div className="form">
         <div className="input">
-          <span className={`input-titel ${formData.login.trim().length > 0 ? "input-full" : ""}`}>first name</span>
+          <span className={`input-titel ${formData.first_name.trim().length > 0 ? "input-full" : ""}`}>first name</span>
           <input
             type="text"
-            value={formData.login}
             onChange={(e) => {
-              setFormData({ ...formData, login: e.target.value });
+              setFormData({ ...formData, first_name: e.target.value });
             }}
           />
         </div>
         <div className="input">
-          <span className={`input-titel ${formData.login.trim().length > 0 ? "input-full" : ""}`}>last name</span>
+          <span className={`input-titel ${formData.last_name.trim().length > 0 ? "input-full" : ""}`}>last name</span>
           <input
             type="text"
-            value={formData.login}
+            value={formData.last_name}
             onChange={(e) => {
-              setFormData({ ...formData, login: e.target.value });
+              setFormData({ ...formData, last_name: e.target.value });
             }}
           />
         </div>
@@ -50,40 +91,49 @@ export default function () {
           <span className={`input-titel ${formData.login.trim().length > 0 ? "input-full" : ""}`}>Email</span>
           <input
             type="text"
-            value={formData.login}
             onChange={(e) => {
               setFormData({ ...formData, login: e.target.value });
             }}
           />
         </div>
         <div className="input">
-          <span className={`input-titel ${formData.login.trim().length > 0 ? "input-full" : ""}`}>CIN</span>
+          <span className={`input-titel ${formData.cin.trim().length > 0 ? "input-full" : ""}`}>CIN</span>
           <input
             type="text"
-            value={formData.login}
             onChange={(e) => {
-              setFormData({ ...formData, login: e.target.value });
+              setFormData({ ...formData, cin: e.target.value });
             }}
           />
         </div>
         <div className="input">
-          <select>
-            <option hidden selected>
-              selectionner le groupe
-            </option>
-            {groups.groups.map((e) => {
-              return <option value={e._id}>{e.name}</option>;
-            })}
-          </select>
+          <span className={`input-titel ${formData.group.trim().length > 0 ? "input-full" : ""}`}>group</span>
+
+          <input
+            type="text"
+            onChange={async (e) => {
+              await handleSearch(e.target.value);
+              setFormData({ ...formData, group: currentSearch[0]._id });
+            }}
+            list="currentsearch"
+          />
         </div>
+        <datalist id="currentsearch">
+          {currentSearch.map((e) => (
+            <option>{e.name}</option>
+          ))}
+        </datalist>
         <input
           type="submit"
-          onClick={async () => {
-            if (formData.login.trim().length < 3 || formData.password.trim().length < 4) return (inputsControle.error_message_info.current.innerHTML = "all fealds are required and password must contains minimum of 4 characters");
-            loadingBarRef.current.classList?.add("active");
-            loadingBarRef.current.classList?.remove("active");
+          onClick={() => {
+            if (formData.login?.trim()?.length < 3 || formData.cin?.trim()?.length < 3 || formData.first_name?.trim()?.length < 3 || formData.last_name?.trim()?.length < 3 || formData.group?.trim()?.length < 3) return (inputsControle.error_message_info.current.innerHTML = "all fealds are required and password must contains minimum of 4 characters");
+            setLoadingFlag({ state: true });
+
+            addStudent(formData.first_name, formData.last_name, formData.cin, formData.login, formData.group, localStorage.getItem(jwt_token))
+              .then(console.log)
+              .then(() => setLoadingFlag({ state: false }));
           }}
-          value="inscrire"
+          value="add"
+          style={{ position: "relative" }}
         />
       </div>
     </div>
--- src/api/index.js
+++ src/api/index.js
@@ -120,19 +120,29 @@ export const getJustificationByID = async (id, token) => {
     });
   return result;
 };
-export const addStudent=async (first_name,last_name,cin,login,group,token)=>{
-  if (!token ) return [true, null];
+export const addStudent = async (first_name, last_name, cin, login, group, token) => {
+  if (!token) return [true, null];
   const result = [null, null];
-  await axios 
-  .get(getServerLink('students/add'), { params: { first_name, last_name,cin,login, group,token,password:cin} })
-  .then((res)=>{
-    result[1]=res.data
-  })
-  .catch((e)=>{
-    result[0]=e
-
-  })
-  return result
-}
-
-
+  await axios
+    .post(getServerLink("students/add"), { first_name, last_name, cin, login, group, token, password: cin })
+    .then((res) => {
+      result[1] = res.data;
+    })
+    .catch((e) => {
+      result[0] = e;
+    });
+  return result;
+};
+export const searchGroupsByName = async (query) => {
+  if (!query) return [true, null];
+  const result = [null, null];
+  await axios
+    .get(getServerLink("groups/searchGroups"), { params: { query } })
+    .then((res) => {
+      result[1] = res.data;
+    })
+    .catch((e) => {
+      result[0] = e;
+    });
+  return result;
+};
