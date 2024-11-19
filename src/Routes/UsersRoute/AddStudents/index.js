a7550ee8fefd4e752cd74033ace585a73a71e526
iliass7777 <iliasstranquille@gmail.com>
2024-11-19 07:38:43 -0800

    yttyt

--- src/Routes/UsersRoute/AddStudents/index.js
+++ src/Routes/UsersRoute/AddStudents/index.js
@@ -1,20 +1,25 @@
 import { useEffect, useRef, useState } from "react";
-import { GroupsDataStore, jwt_token } from "../../../data";
-import { getGroups } from "../../../api/index";
+import { GroupsDataStore, jwt_token, loadingFlag, userDataStore } from "../../../data";
+import { addStudent, getGroups } from "../../../api/index";
 import { searchGroupsByName } from "../../../api/index";
+import "./index.css";
 export default function () {
   const [groups, setGroups] = GroupsDataStore.useStore();
   const [formData, setFormData] = useState({
     login: "",
-    password: "",
+    first_name: "",
+    last_name: "",
+    cin: "",
+    group: "",
   });
-  const loadingBarRef = useRef("");
+  const [userData, setUserData] = userDataStore.useStore();
+  const [loading, setLoadingFlag] = loadingFlag.useStore();
   const inputsControle = {
     error_message_info: useRef(""),
   };
   const [search, setSearch] = useState([]);
   useEffect(() => {
-    getGroups(window.localStorage.getItem(jwt_token), false).then((res) => {
+    getGroups(userData.token, false).then((res) => {
       if (res[0]) return;
       const exist = [];
       setGroups(
@@ -39,43 +44,52 @@ export default function () {
     }
     const [error, data] = await searchGroupsByName(value);
     if (error) return console.error("Error searching groups:", error);
-    setSearch(data);
+    setSearch({ ...search, [value.toLowerCase().trim()]: data, keys: [...search.keys, value.toLowerCase().trim()] });
     console.log("Search results server:", data);
   };
   return (
-    <div className="login">
-      <div className="loading-bar" ref={loadingBarRef}></div>
+    <div className="add-student">
       <div>
         <span className="error" ref={inputsControle.error_message_info}></span>
         <h1>inscription</h1>
       </div>
       <div className="form">
         <div className="input">
-          <span className={`input-titel ${formData.login.trim().length > 0 ? "input-full" : ""}`}>first name</span>
+          <span className={`input-titel ${formData.first_name.trim().length > 0 ? "input-full" : ""}`}>first name</span>
           <input
             type="text"
             onChange={(e) => {
-              setFormData({ ...formData, login: e.target.value });
+              setFormData({ ...formData, first_name: e.target.value });
             }}
           />
         </div>
         <div className="input">
-          <span className={`input-titel ${formData.login.trim().length > 0 ? "input-full" : ""}`}>last name</span>
+          <span className={`input-titel ${formData.last_name.trim().length > 0 ? "input-full" : ""}`}>last name</span>
           <input
             type="text"
-            value={formData.login}
+            value={formData.last_name}
             onChange={(e) => {
-              setFormData({ ...formData, login: e.target.value });
+              setFormData({ ...formData, last_name: e.target.value });
             }}
           />
         </div>
         <div className="input">
           <span className={`input-titel ${formData.login.trim().length > 0 ? "input-full" : ""}`}>Email</span>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(e) => {
+              setFormData({ ...formData, login: e.target.value });
+            }}
+          />
         </div>
         <div className="input">
-          <span className={`input-titel ${formData.login.trim().length > 0 ? "input-full" : ""}`}>CIN</span>
-          <input type="text" />
+          <span className={`input-titel ${formData.cin.trim().length > 0 ? "input-full" : ""}`}>CIN</span>
+          <input
+            type="text"
+            onChange={(e) => {
+              setFormData({ ...formData, cin: e.target.value });
+            }}
+          />
         </div>
         <div className="input">
           <select>
@@ -87,14 +101,23 @@ export default function () {
             })}
           </select>
         </div>
+        <datalist id="currentsearch">
+          {currentSearch.map((e) => (
+            <option>{e.name}</option>
+          ))}
+        </datalist>
         <input
           type="submit"
-          onClick={async () => {
-            if (formData.login.trim().length < 3 || formData.password.trim().length < 4) return (inputsControle.error_message_info.current.innerHTML = "all fealds are required and password must contains minimum of 4 characters");
-            loadingBarRef.current.classList?.add("active");
-            loadingBarRef.current.classList?.remove("active");
+          onClick={() => {
+            if (formData.login?.trim()?.length < 3 || formData.cin?.trim()?.length < 3 || formData.first_name?.trim()?.length < 3 || formData.last_name?.trim()?.length < 3 || formData.group?.trim()?.length < 3) return (inputsControle.error_message_info.current.innerHTML = "all fealds are required and password must contains minimum of 4 characters");
+            setLoadingFlag({ state: true });
+
+            addStudent(formData.first_name, formData.last_name, formData.cin, formData.login, formData.group, userData.token)
+              .then(console.log)
+              .then(() => setLoadingFlag({ state: false }));
           }}
-          value="inscrire"
+          value="add"
+          style={{ position: "relative" }}
         />
       </div>
     </div>
